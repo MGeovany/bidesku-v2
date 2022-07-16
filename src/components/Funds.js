14606c0daa8aa82c1de630673874c2835bc71dc5
Geovany <marlongeo1999@gmail.com>
2022-07-15 21:49:00 -0600

    🗯️ all layout completed

--- src/components/Funds.js
+++ src/components/Funds.js
@@ -1,5 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+// import faker from 'faker'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      align: 'start',
+      boxHeight: 30,
+      padding: 30,
+      usePointStyle: true,
+      pointStyle: 'circle'
+    }
+  }
+}
 
 export const Funds = () => {
-  return <div>Funds</div>
+  const [age, setAge] = useState(72)
+
+  const labels = [age, 'Age 75', 'Age 85', 'Age 90']
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Index Funds Alone',
+        data: [190000, 240000, 430000, 575000],
+        backgroundColor: 'rgb(155,164,179)'
+      },
+      {
+        label: 'With BiDeskU',
+        data: [243000, 323000, 1055000, 2710000],
+        backgroundColor: 'rgb(0,209,178)'
+      }
+    ]
+  }
+
+  return (
+    <div className='estimator__card__container'>
+      <div className='card estimator__card__container'>
+        <div className='card-content'>
+          <Bar options={options} data={data} />
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/ShareEstimator.js
+++ src/components/ShareEstimator.js
@@ -2,26 +2,24 @@ import React from 'react'
 
 export const ShareEstimator = () => {
   return (
-    <div>
-      <div className='card'>
-        <div className='card-content'>
-          <section className='email__input '>
-            <input
-              className='input is-primary'
-              type='text'
-              placeholder='i.e.client@email.com'
-            />
-          </section>
+    <div className='card estimator__card__container is-flex is-flex-direction-column is-justify-content-center'>
+      <div className='card-content '>
+        <section className='email__input '>
+          <input
+            className='input is-success'
+            type='text'
+            placeholder='i.e.client@email.com'
+          />
+        </section>
 
-          <section className='email__btns is-flex is-flex-direction-row is-justify-content-space-between py-3'>
-            <button className='button is-primary is-outlined mr-1'>
-              <span>Share via Email</span>
-            </button>
-            <button className='button is-info is-inverted'>
-              <span>Download PDF</span>
-            </button>
-          </section>
-        </div>
+        <section className='email__btns is-flex is-flex-direction-row is-justify-content-space-between py-3'>
+          <button className='button is-primary is-outlined mr-1'>
+            <span>Share via Email</span>
+          </button>
+          <button className='button is-info is-inverted'>
+            <span>Download PDF</span>
+          </button>
+        </section>
       </div>
     </div>
   )
